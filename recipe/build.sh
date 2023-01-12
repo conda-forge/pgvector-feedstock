d62cacb002712cc528379e113765687d67647cae
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-01-11 21:38:20 -0800

    Bump to 0.4.0 and update build system to use nmake on windows (#6)
    * Bump to 0.4.0 and update build system to use nmake on windows
    
    * Add support for osx_arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -ex
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
+
 make
 make install
 
 initdb -D test_db
 pg_ctl -D test_db -l test.log start
 
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+
 make installcheck
+
+fi
 
 pg_ctl -D test_db stop
 
