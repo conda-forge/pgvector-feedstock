74006c0f48714164ec02d60486852eae668bc27a
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-01-11 19:44:07 -0800

    Skip installcheck on cross compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,12 @@ make install
 initdb -D test_db
 pg_ctl -D test_db -l test.log start
 
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+
 make installcheck
+
+fi
 
 pg_ctl -D test_db stop
 
