7a7fa3d6576bb44077e6658ab5a073380275cbd8
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-02-08 12:24:23 -0800

    Add tests for osx_arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,15 +8,15 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 make
 make install
 
-initdb -D test_db
-pg_ctl -D test_db -l test.log start
-
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
 
+initdb -D test_db
+pg_ctl -D test_db -l test.log start
+
 make installcheck
 
-fi
-
 pg_ctl -D test_db stop
+
+fi
 
