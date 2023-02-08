2453b09755abd8faac8d4c827d9e0ab3cccd998c
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-02-08 13:00:58 -0800

    Fix PGROOT

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -ex
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
   export PGROOT="${PREFIX}"
 fi
 
