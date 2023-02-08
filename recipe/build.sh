bca95cb9fa7a8d21a46a9afeb5f43bea982b30d1
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-02-08 14:52:27 -0800

    Fix and add tests for osx_arm64 build (#7)

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,19 +4,30 @@ set -ex
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+  export PGROOT="${PREFIX}"
+fi
 
 make
-make install
 
-initdb -D test_db
-pg_ctl -D test_db -l test.log start
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+  # Manually installing files because pgvector does auto-detection of install directory using pgxs in $BUILD_PREFIX
+  /usr/bin/install -c -m 755 vector.so "${PREFIX}/lib/vector.so"
+  /usr/bin/install -c -m 644 vector.control "${PREFIX}/share/extension/"
+  /usr/bin/install -c -m 644 sql/* "${PREFIX}/share/extension/"
+else
+  make install
+fi
 
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
 
+initdb -D test_db
+pg_ctl -D test_db -l test.log start
+
 make installcheck
 
-fi
-
 pg_ctl -D test_db stop
+
+fi
 
