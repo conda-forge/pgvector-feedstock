b84bd35b28c1a026e50607be21469ee92aa01eb8
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-02-08 14:33:56 -0800

    Clean up and comment

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,12 +11,10 @@ fi
 make
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+  # Manually installing files because pgvector does auto-detection of install directory using pgxs in $BUILD_PREFIX
   /usr/bin/install -c -m 755 vector.so "${PREFIX}/lib/vector.so"
   /usr/bin/install -c -m 644 vector.control "${PREFIX}/share/extension/"
   /usr/bin/install -c -m 644 sql/* "${PREFIX}/share/extension/"
-  ls
-  ls $PREFIX/share/extension
-  ls $BUILD_PREFIX/share/extension
 else
   make install
 fi
