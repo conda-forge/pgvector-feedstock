af15dab141415b8aff7b18f8aae9f264cfd1aaa5
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-02-10 14:18:09 -0800

    working now but might break it

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
   chmod +x $RECIPE_DIR/arm64_pg_config
   export PG_CONFIG="${RECIPE_DIR}/arm64_pg_config"
-  make DESTDIR="${PREFIX}"
+  make DESTDIR="${PREFIX}" OPTFLAGS=""
   make install
 
 else
