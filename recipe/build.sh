ba3bfa753d463bfae168d94473e13b8df71edce2
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-02-10 10:07:32 -0800

    Another attempt

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,7 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
   export PG_CONFIG="${PREFIX}/bin/pg_config"
-  make CC="${CC_FOR_BUILD}"
+  make CC="${CC}"
   make install
 
 else
